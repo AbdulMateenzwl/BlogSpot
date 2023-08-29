18a749a926d87672f4687ee72e7fadaafa278e61
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2023-08-29 10:24:06 -0700

    NavBar UI Updated

--- app/NavMenu.tsx
+++ app/NavMenu.tsx
@@ -1,27 +1,43 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './NavMenu.module.css';
-import { SignInButton } from '@/components/Buttons';
+import {
+	SignInButton,
+	SignOutButton,
+	LoggedUserData,
+} from '@/components/Buttons';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
+import { authOptions } from './api/auth/[...nextauth]/route';
 
-export default function NavMenu() {
+
+export default async function NavMenu() {
+	const session = await getServerSession(authOptions);
+
 	return (
-		<nav className={styles.nav}>
+		<nav className='flex bg-green-400 justify-between'>
 			<Link href={'/'}>
-				<Image src='/next.svg' width={216} height={30} alt='logo' className='mx-2 scale-50 sm:scale-100' />
+				<Image
+					src='/next.svg'
+					width={216}
+					height={30}
+					alt='logo'
+					className='mx-2 scale-50 sm:scale-100'
+				/>
 			</Link>
 
-			<ul className={styles.links}>
-				<li>
+			<ul className='flex h-[50px] '>
+				<li className='text-lg m-1 mx-2 bg-green-800 hover:bg-green-50'>
 					<Link href={'/about'}>About</Link>
 				</li>
-				<li>
+				<li className='text-lg m-1 mx-2 bg-green-800 hover:bg-green-50'>
 					<Link href={'/blog'}>Blog</Link>
 				</li>
-				<li>
+				<li className='text-lg m-1 mx-2 bg-green-800 hover:bg-green-50'>
 					<Link href={'/users'}>Users</Link>
 				</li>
-				<li>
-					<SignInButton/>
+				<li>{session && <LoggedUserData />}</li>
+				<li className='text-lg m-1 mx-2 bg-green-800 hover:bg-green-50'>
+					{session !== null ? <SignOutButton /> : <SignInButton />}
 				</li>
 			</ul>
 		</nav>
--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -4,7 +4,7 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export function SignInButton() {
+export function LoggedUserData() {
 	const { data: session, status } = useSession();
 
 	if (status === 'loading') {
@@ -13,32 +13,38 @@ export function SignInButton() {
 
 	if (status === 'authenticated') {
 		return (
-			<>
+			<div>
 				<Link href={'/dashboard'}>
 					<Image
-                    className='rounded-full'
+						className='rounded-full'
 						src={session.user?.image ?? '/user.jpg'}
 						alt='user image'
 						width={32}
 						height={32}
 					></Image>
 				</Link>
-				<SignOutButton />
-			</>
+			</div>
 		);
 	}
+}
 
+export function SignInButton() {
 	return (
-		<button onClick={() => signIn()} className='mx-2 p-2 bg-blue-800 rounded-xl'>
+		<button
+			onClick={() => signIn()}
+			className='mx-2 p-2 bg-blue-800 rounded-xl'
+		>
 			Sign In
 		</button>
 	);
 }
 
 export function SignOutButton() {
-
 	return (
-		<button onClick={() => signOut()} className='mx-2 p-2 bg-blue-800 rounded-xl'>
+		<button
+			onClick={() => signOut()}
+			className='mx-1 sm:mx-2 p-2 bg-green-800 rounded-xl'
+		>
 			Sign Out
 		</button>
 	);
