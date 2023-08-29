6f36c21d60a2db0e86dacb67a69e4e52f362a4d3
abdulmateenzwl <abdulmateenkhanzwl@gmail.com>
2023-08-29 03:31:36 -0700

    prisma follow schema,setup

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,27 @@
+import {SignOutButton} from '@/components/Buttons'
+import {authOptions} from "../api/auth/[...nextauth]/route"
+import { getServerSession } from 'next-auth'
+import {redirect} from 'next/navigation'
+import { prisma } from '@/lib/prisma'
+import { ProfileForm } from './ProfileForm'
+export default async function DashBoard(){
+    const session = await getServerSession(authOptions)
+
+    if(!session){
+        redirect('/api/auth/signin');
+    }
+
+    const currentUserEmail = session?.user?.email!;
+    const user = await prisma.user.findUnique({
+where:{
+    email:currentUserEmail
+}
+    })
+
+    return (
+        <>
+        <h1>Dashboard</h1>
+        <ProfileForm user={user}/>
+        </>
+    )
+}
